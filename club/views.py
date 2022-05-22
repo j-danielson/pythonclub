a8c8c8cc072ffe7c47ae0fe335a8ba8065cbdd5d
Josh Danielson <37751792+j-danielson@users.noreply.github.com>
2022-05-22 10:21:02 -0700

    Completed meetingdetails page

--- club/views.py
+++ club/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +9,11 @@ def index(request):
 def resources(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resources.html', {'resource_list': resource_list})
+
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_list': meeting_list})    
+
+def meetingdetail(request, id):
+    meetingdetail=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetail.html', {'meetingdetail' : meetingdetail})
